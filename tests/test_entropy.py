19c250a38b0e2f5b795ad8ea544f386a6929d489
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2016-01-11 14:59:34 +0100

    refactor entropy tests

--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -15,40 +15,34 @@ def assert_length(collection, length):
 assert_length.__test__ = False
 
 
-def test_entropy_instantiation():
-    _entropy.Entropy()
+class TestEntropy:
 
+    def setup(self):
+        # pylint: disable=attribute-defined-outside-init
+        # pylint: disable=invalid-name
+        self.s = _entropy.Entropy()
 
-def test_entropy_gather():
-    entropy = _entropy.Entropy()
-    entropy.gather()
+    def test_gather(self):
+        # Only test that this does not raise.
+        self.s.gather()
 
+    def test_retrieve(self):
+        for length in range(64):
+            assert_length(self.s.retrieve(length), length)
 
-def test_entropy_retrieve():
-    entropy = _entropy.Entropy()
-    for length in range(64):
-        assert_length(entropy.retrieve(length), length)
+    @raises(EntropySourceError)
+    def test_retrieve_long_block_raises(self):
+        self.s.retrieve(100)
 
+    def test_update(self):
+        # Only test that this does not raise.
+        buf = _rnd(64)
+        self.s.update(buf)
 
-@raises(EntropySourceError)
-def test_retrieve_long_entropy_block_raises():
-    entropy = _entropy.Entropy()
-    entropy.retrieve(100)
+    def test_not_reproducible(self):
+        assert_not_equal(self.s.retrieve(8), self.s.retrieve(8))
 
-
-def test_entropy_update():
-    # Only test that this does not raise.
-    entropy = _entropy.Entropy()
-    buf = _rnd(64)
-    entropy.update(buf)
-
-
-def test_not_reproducible():
-    entropy = _entropy.Entropy()
-    assert_not_equal(entropy.retrieve(8), entropy.retrieve(8))
-
-
-def test_random_initial_values():
-    s1 = _entropy.Entropy()
-    s2 = _entropy.Entropy()
-    assert_not_equal(s1.retrieve(8), s2.retrieve(8))
+    def test_random_initial_values(self):
+        # pylint: disable=invalid-name
+        s = _entropy.Entropy()
+        assert_not_equal(self.s.retrieve(8), s.retrieve(8))
