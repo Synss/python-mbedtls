844c9db7e9a5ca5af2c304055a4668fc352b775c
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2016-01-11 10:02:13 +0100

    implement random generator

--- mbedtls/random.pyx
+++ mbedtls/random.pyx
@@ -5,15 +5,54 @@ __copyright__ = "Copyright 2016, Elaborated Networks GmbH"
 __license__ = "Apache License 2.0"
 
 
+from libc.stdlib cimport malloc, free
+cimport entropy
 cimport random
+import binascii
+from mbedtls.exceptions import check_error
 
 
 cdef class Random:
 
+    cdef mbedtls_ctr_drbg_context _ctx
+    cdef entropy.Entropy _entropy
+
     def __cinit__(self):
         """Initialize the context."""
         random.mbedtls_ctr_drbg_init(&self._ctx)
+        self._entropy = entropy.Entropy()
+        check_error(random.mbedtls_ctr_drbg_seed(
+            &self._ctx,
+            &entropy.mbedtls_entropy_func, &self._entropy._ctx,
+            NULL, 0))
 
     def __dealloc__(self):
         """Free and clear the context."""
         random.mbedtls_ctr_drbg_free(&self._ctx)
+
+    cpdef reseed(self):
+        """Reseed the RNG."""
+        check_error(random.mbedtls_ctr_drbg_reseed(&self._ctx, NULL, 0))
+
+    cpdef update(self, data):
+        """Update state with additional data."""
+        cdef unsigned char[:] c_data = bytearray(data)
+        random.mbedtls_ctr_drbg_update(&self._ctx, &c_data[0], c_data.shape[0])
+
+    cpdef token_bytes(self, length):
+        """Returns `length` random bytes."""
+        cdef size_t sz = length
+        cdef unsigned char* output = <unsigned char*>malloc(
+            sz * sizeof(unsigned char))
+        if not output:
+            raise MemoryError()
+        try:
+            check_error(random.mbedtls_ctr_drbg_random(
+                &self._ctx, output, sz))
+            return bytes([output[n] for n in range(sz)])
+        finally:
+            free(output)
+
+    cpdef token_hex(self, length):
+        """Same as `token_bytes` but returned as a string."""
+        return binascii.hexlify(self.token_bytes(length)).decode("ascii")
--- tests/test_random.py
+++ tests/test_random.py
@@ -1,8 +1,43 @@
 """Unit tests for mbedtls.random."""
+# pylint: disable=missing-docstring
 
 
-import mbedtls.random as rnd
+# pylint: disable=import-error
+import mbedtls.random as _drbg
+# pylint: enable=import-error
+from nose.tools import assert_equal, assert_not_equal
 
 
-def test_instantiation():
-    rnd.Random()
+def assert_length(collection, length):
+    assert_equal(len(collection), length)
+assert_length.__test__ = False
+
+
+class TestRandom:
+
+    def setup(self):
+        # pylint: disable=attribute-defined-outside-init
+        self.rnd = _drbg.Random()
+
+    def test_reseed(self):
+        self.rnd.reseed()
+
+    def test_not_reproducible(self):
+        assert_not_equal(self.rnd.token_bytes(8),
+                         self.rnd.token_bytes(8))
+
+    def test_update(self):
+        self.rnd.update(b"additional data")
+
+    def test_initial_values(self):
+        rnd = _drbg.Random()
+        assert_not_equal(self.rnd.token_bytes(8),
+                         rnd.token_bytes(8))
+
+    def test_token_bytes(self):
+        for length in range(1024):
+            assert_length(self.rnd.token_bytes(length), length)
+
+    def test_token_hex(self):
+        for length in range(1024):
+            assert_length(self.rnd.token_hex(length), 2 * length)
