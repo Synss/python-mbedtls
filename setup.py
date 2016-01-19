b34761a5c0316fa919bcc68171cab86b3ef875b3
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2016-01-19 11:34:10 +0100

    package files for 0.4 release

--- setup.py
+++ setup.py
@@ -6,31 +6,28 @@ download_url = "https://github.com/Synss/python-mbedtls/tarball/%s" % version
 
 
 extensions = [
-    Extension("mbedtls.exceptions", ["mbedtls/exceptions.pyx"]),
+    Extension("mbedtls.exceptions", ["mbedtls/exceptions.c"]),
 ] + [
-    Extension("mbedtls.random", ["mbedtls/random.pyx"],
+    Extension("mbedtls.random", ["mbedtls/random.c"],
               libraries=["mbedtls"],
               include_dirs=["."],)
 ] + [
-    Extension("mbedtls.cipher.%s" % name, ["mbedtls/cipher/%s.pyx" % name],
+    Extension("mbedtls.cipher.%s" % name, ["mbedtls/cipher/%s.c" % name],
               libraries=["mbedtls"],
               include_dirs=["."],) for name in
     "_cipher __init__".split() +
     "AES ARC4 Blowfish Camellia DES DES3 DES3dbl".split()
 ] + [
-    Extension("mbedtls.pk.%s" % name, ["mbedtls/pk/%s.pyx" % name],
+    Extension("mbedtls.pk.%s" % name, ["mbedtls/pk/%s.c" % name],
               libraries=["mbedtls"],
               include_dirs=["."],) for name in
     "_pk __init__ RSA".split()
 ] + [
-    Extension("mbedtls.%s" % name, ["mbedtls/%s.pyx" % name],
+    Extension("mbedtls.%s" % name, ["mbedtls/%s.c" % name],
               libraries=["mbedtls"],
               include_dirs=["."],)
     for name in "_md __init__ hash hmac".split()
 ]
-
-setup_requires = ["cython"]
-
 
 setup(
     name="python-mbedtls",
@@ -42,7 +39,6 @@ setup(
     url="https://synss.github.io/python-mbedtls",
     download_url=download_url,
     ext_modules=extensions,
-    setup_requires=setup_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Cython",
