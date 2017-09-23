d49c28ddeacf17cd0673d5bc5ead4ab9cc9c9055
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2017-09-23 23:32:47 +0200

    Convert `tests.__init__` to pytest: delete tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
+# pylint: disable=missing-docstring
+
 import random
 
 
@@ -10,28 +12,13 @@ def assert_canonical_repr(obj):
         # pylint: disable=eval-used
         newobj = eval(repr(obj), frame.f_globals, frame.f_locals)
     except TypeError:
-        raise AssertionError("Cannot eval '%r'" % obj) from None
+        raise AssertionError("Cannot eval '%r'" % obj)
     finally:
         # explicitely delete the frame to avoid memory leaks, see also
         # https://docs.python.org/3/library/inspect.html#the-interpreter-stack
         del frame
     assert isinstance(newobj, type(obj))
-assert_canonical_repr.__test__ = False
 
 
 def _rnd(length):
     return bytes(random.randrange(0, 256) for _ in range(length))
-_rnd.__test__ = False
-
-
-class TestRnd:
-
-    @staticmethod
-    def test_key_length():
-        for length in range(1024 + 1, 8):
-            assert len(_rnd(length)) == length
-
-    @staticmethod
-    def test_values_fit_in_latin1():
-        k = _rnd(2048)
-        assert k.decode("latin1")
