570546d7d8ea13b7cdc2118315dca15e2b8d66d9
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2018-10-10 23:09:42 +0200

    mpi: Fixup ref count erase secrets

--- mbedtls/mpi.pyx
+++ mbedtls/mpi.pyx
@@ -50,18 +50,15 @@ cdef class MPI:
             value = to_bytes(value)
             self._from_bytes(value)
 
-    def __del__(self):
-        """Fill the MPI with random data."""
-        check_error(mbedtls_mpi_fill_random(
-            &self._ctx, self._len(),
-            &_random.mbedtls_ctr_drbg_random, &__rng._ctx))
-
     def __cinit__(self):
         """Initialize one MPI."""
         _mpi.mbedtls_mpi_init(&self._ctx)
 
     def __dealloc__(self):
         """Unallocate one MPI."""
+        check_error(mbedtls_mpi_fill_random(
+            &self._ctx, self._len(),
+            &_random.mbedtls_ctr_drbg_random, &__rng._ctx))
         _mpi.mbedtls_mpi_free(&self._ctx)
 
     cdef size_t _len(self):
