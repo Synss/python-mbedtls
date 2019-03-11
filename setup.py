7074bc0660b09bccc03600ca32646fa7ae53896e
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2019-03-11 20:21:23 +0100

    conf: Increase version to 0.15.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, Extension, find_packages
 
-version = "0.14.1"
+version = "0.15.0"
 download_url = "https://github.com/Synss/python-mbedtls/tarball/%s" % version
 
 
