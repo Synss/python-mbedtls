49adc0d083f9c9b85ad4fec80e1bd277a3ce4f2e
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2019-05-11 10:27:37 +0200

    ci: Move `requirements*` files to dir

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ set -ex
 echo "Index: $PIP_INDEX_URL"
 
 for pydir in /opt/python/*; do
-	$pydir/bin/python -m pip install -r requirements-tests.txt
+	$pydir/bin/python -m pip install -r requirements/tests.txt
 	$pydir/bin/python -m pip install \
 		--only-binary=:all: \
 		python-mbedtls
