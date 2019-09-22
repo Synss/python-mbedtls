b64920cab540ffc1d556ebc302cf83b2b730db13
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2019-09-22 22:25:53 +0200

    setup.py: Refuse to build if mbedtls is too old
    This closes Issue #26.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import ctypes
 import os
 import sys
 from setuptools import setup, Extension, find_packages
 
 version = "0.18.2"
+mbedtls_version = "2.16.2"
 download_url = "https://github.com/Synss/python-mbedtls/tarball/%s" % version
+
+__mbedtls_version_info__ = tuple(map(int, mbedtls_version.split(".")))
+__mbedtls_url__ = "https://tls.mbed.org"
 
 
 if "--with-coverage" in sys.argv:
@@ -25,6 +30,62 @@ if sys.version_info < (3,):
 tests_require = ["readme_renderer"]
 if sys.version_info < (3,):
     tests_require.extend(["contextlib2"])
+
+
+def mbedtls_version(lib):
+    null = b"\0"
+    output = 18 * null
+    output_p = ctypes.c_char_p(output)
+    lib.mbedtls_version_get_string_full(output_p)
+    return output.strip(null).decode("ascii")
+
+
+def mbedtls_version_info(lib):
+    version = lib.mbedtls_version_get_number()
+    return tuple(version >> shift & 0xFF for shift in (24, 16, 8))
+
+
+def load_mbedtls():
+    if sys.platform.startswith("linux"):
+        name = "libmbedtls.so"
+    elif sys.platform == "darwin":
+        name = "libmbedtls.dylib"
+    else:
+        return None
+    return ctypes.cdll.LoadLibrary(name)
+
+
+def check_mbedtls_support(version, url):
+    try:
+        lib = load_mbedtls()
+        if not lib:
+            # I do not know the other platforms, so let us
+            # skip the check for now.
+            return
+        sys.stdout.write(
+            "  mbedtls version: {!s}{sep}".format(
+                mbedtls_version(lib), sep=os.linesep
+            )
+        )
+    except OSError:
+        lib = None
+        sys.stderr.write(
+            "  mbedtls not found: could not load shared library{sep}".format(
+                sep=os.linesep
+            )
+        )
+    if lib is None or mbedtls_version_info(lib) < version[:2]:
+        message = (
+            "  python-mbedtls requires at least mbedtls {major}.{minor}".format(
+                major=version[0], minor=version[1]
+            ),
+            "  The latest version of mbedtls may be obtained from {url}.".format(
+                url=url
+            ),
+            "",
+        )
+        sys.stderr.writelines(os.linesep.join(message))
+        sys.exit(1)
 
 
 def extensions(coverage=False):
@@ -68,6 +129,14 @@ def options(coverage=False):
 def readme():
     with open("README.rst") as f:
         return f.read().replace(":math:", "")
+
+
+if len(sys.argv) > 1 and any(
+    (sys.argv[1].startswith("build"), sys.argv[1].startswith("bdist"))
+):
+    check_mbedtls_support(
+        version=__mbedtls_version_info__, url=__mbedtls_url__
+    )
 
 
 setup(
