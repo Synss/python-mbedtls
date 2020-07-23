84e5019db26c3be02599d7f45f618dbb8e2cde30
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2020-07-23 21:58:03 +0200

    ci: Increase version to 1.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2016, Mathias Laurin"
 author = "Mathias Laurin"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0"
+release = "1.3.1"
 
 
 # -- General configuration ---------------------------------------------------
