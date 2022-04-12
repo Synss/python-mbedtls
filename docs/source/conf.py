f148e91f2534c471036323f86926da2614205c8b
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-04-12 17:13:31 +0200

    ci: Increase version to 2.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2016, Mathias Laurin"
 author = "Mathias Laurin"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.0.1"
 
 
 # -- General configuration ---------------------------------------------------
