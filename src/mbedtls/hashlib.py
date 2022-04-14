a69487297dca4715e3c5d271b0aed2cee8d65cfb
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-04-14 10:04:38 +0200

    md: Always call new()
    There is not performance difference in our implementation and having
    a single entry point in Python eases maintenance.

--- src/mbedtls/hashlib.py
+++ src/mbedtls/hashlib.py
@@ -24,45 +24,45 @@ def new(name, buffer=None):
 
 def md2(buffer=None):
     """MD2 message-digest algorithm."""
-    return Hash("md2", buffer)
+    return new("md2", buffer)
 
 
 def md4(buffer=None):
     """MD4 message-digest algorithm."""
-    return Hash("md4", buffer)
+    return new("md4", buffer)
 
 
 def md5(buffer=None):
     """MD5 message-digest algorithm."""
-    return Hash("md5", buffer)
+    return new("md5", buffer)
 
 
 def sha1(buffer=None):
     """Secure Hash Algorithm 1 (SHA-1)."""
-    return Hash("sha1", buffer)
+    return new("sha1", buffer)
 
 
 def sha224(buffer=None):
     """Secure Hash Algorithm 2 (SHA-2) with 224 bits hash value."""
-    return Hash("sha224", buffer)
+    return new("sha224", buffer)
 
 
 def sha256(buffer=None):
     """Secure Hash Algorithm 2 (SHA-2) with 256 bits hash value."""
-    return Hash("sha256", buffer)
+    return new("sha256", buffer)
 
 
 def sha384(buffer=None):
     """Secure Hash Algorithm 2 (SHA-2) with 384 bits hash value."""
-    return Hash("sha384", buffer)
+    return new("sha384", buffer)
 
 
 def sha512(buffer=None):
     """Secure Hash Algorithm 2 (SHA-2) with 512 bits hash value."""
-    return Hash("sha512", buffer)
+    return new("sha512", buffer)
 
 
 def ripemd160(buffer=None):
     """RACE Integrity Primitives Evaluation Message Digest (RIPEMD) with
     160 bits hash value."""
-    return Hash("ripemd160", buffer)
+    return new("ripemd160", buffer)
--- src/mbedtls/hmac.py
+++ src/mbedtls/hmac.py
@@ -26,45 +26,45 @@ def new(key, buffer=None, digestmod=None):
 
 def md2(key, buffer=None):
     """MD2 message-digest algorithm."""
-    return Hmac(key, "md2", buffer)
+    return new(key, buffer, "md2")
 
 
 def md4(key, buffer=None):
     """MD4 message-digest algorithm."""
-    return Hmac(key, "md4", buffer)
+    return new(key, buffer, "md4")
 
 
 def md5(key, buffer=None):
     """MD5 message-digest algorithm."""
-    return Hmac(key, "md5", buffer)
+    return new(key, buffer, "md5")
 
 
 def sha1(key, buffer=None):
     """Secure Hmac Algorithm 1 (SHA-1)."""
-    return Hmac(key, "sha1", buffer)
+    return new(key, buffer, "sha1")
 
 
 def sha224(key, buffer=None):
     """Secure Hmac Algorithm 2 (SHA-2) with 224 bits hash value."""
-    return Hmac(key, "sha224", buffer)
+    return new(key, buffer, "sha224")
 
 
 def sha256(key, buffer=None):
     """Secure Hmac Algorithm 2 (SHA-2) with 256 bits hash value."""
-    return Hmac(key, "sha256", buffer)
+    return new(key, buffer, "sha256")
 
 
 def sha384(key, buffer=None):
     """Secure Hmac Algorithm 2 (SHA-2) with 384 bits hash value."""
-    return Hmac(key, "sha384", buffer)
+    return new(key, buffer, "sha384")
 
 
 def sha512(key, buffer=None):
     """Secure Hmac Algorithm 2 (SHA-2) with 512 bits hash value."""
-    return Hmac(key, "sha512", buffer)
+    return new(key, buffer, "sha512")
 
 
 def ripemd160(key, buffer=None):
     """RACE Integrity Primitives Evaluation Message Digest (RIPEMD) with
     160 bits hash value."""
-    return Hmac(key, "ripemd160", buffer)
+    return new(key, buffer, "ripemd160")
