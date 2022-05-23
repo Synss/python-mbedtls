d6fc3fa2811c5ef6ab347a7cb3eb84ed0d016aee
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-05-24 00:00:09 +0200

    test_md: Table-based tests

--- tests/test_md.py
+++ tests/test_md.py
@@ -7,23 +7,31 @@
 
 import pickle
 from collections.abc import Collection
+from typing import Mapping, NamedTuple
 
 import pytest  # type: ignore
 
-import mbedtls
 from mbedtls import hashlib
 from mbedtls import hmac as hmaclib
 from mbedtls._md import Hash  # type: ignore
 
 
-def make_chunks(buffer, size):
-    for i in range(0, len(buffer), size):
-        yield buffer[i : i + size]
+class Size(NamedTuple):
+    digest_size: int
+    block_size: int
 
 
-def test_make_chunks(randbytes):
-    buffer = randbytes(1024)
-    assert b"".join(buf for buf in make_chunks(buffer, 100)) == buffer
+SUPPORTED_SIZES: Mapping[str, Size] = {
+    "md2": Size(digest_size=16, block_size=16),
+    "md4": Size(digest_size=16, block_size=64),
+    "md5": Size(digest_size=16, block_size=64),
+    "sha1": Size(digest_size=20, block_size=64),
+    "sha224": Size(digest_size=28, block_size=64),
+    "sha256": Size(digest_size=32, block_size=64),
+    "sha384": Size(digest_size=48, block_size=128),
+    "sha512": Size(digest_size=64, block_size=128),
+    "ripemd160": Size(digest_size=20, block_size=64),
+}
 
 
 def test_algorithms():
@@ -48,401 +56,107 @@ def test_unavailable_cipher_raises_ValueError():
         Hash("unavailable")
 
 
-class _TestMDBase:
-    @pytest.fixture
-    def algorithm(self):
-        raise NotImplementedError
-
-    @pytest.fixture
-    def digest_size(self):
-        raise NotImplementedError
-
-    @pytest.fixture
-    def block_size(self):
-        raise NotImplementedError
+class TestHash:
+    @pytest.fixture(params=tuple(hashlib.algorithms_available))
+    def algorithm(self, request):
+        return request.param
 
     @pytest.mark.parametrize("repr_", (repr, str), ids=lambda f: f.__name__)
     def test_repr(self, repr_, algorithm):
-        assert isinstance(repr_(algorithm), str)
+        assert isinstance(repr_(hashlib.new(algorithm, b"")), str)
 
     def test_pickle(self, algorithm):
         with pytest.raises(TypeError) as excinfo:
-            pickle.dumps(algorithm)
+            pickle.dumps(hashlib.new(algorithm, b""))
 
         assert str(excinfo.value).startswith("cannot pickle")
 
-    def test_digest_size_accessor(self, algorithm, digest_size):
-        assert algorithm.digest_size == digest_size
-
-    def test_digest_size(self, algorithm, digest_size):
-        assert len(algorithm.digest()) == digest_size
-
-    def test_block_size_accessor(self, algorithm, block_size):
-        assert algorithm.block_size == block_size
-
-
-class _TestHash(_TestMDBase):
-    @pytest.fixture
-    def buffer(self, randbytes):
-        return randbytes(512)
-
-    def test_new(self, algorithm, buffer):
-        copy = hashlib.new(algorithm.name, buffer)
-        algorithm.update(buffer)
-        assert algorithm.digest() == copy.digest()
-        assert algorithm.hexdigest() == copy.hexdigest()
-
-    def test_copy_and_update(self, algorithm, buffer):
-        copy = algorithm.copy()
-        algorithm.update(buffer)
-        copy.update(buffer)
-        assert algorithm.digest() == copy.digest()
-        assert algorithm.hexdigest() == copy.hexdigest()
-
-    def test_copy_and_update_nothing(self, algorithm, buffer):
-        copy = algorithm.copy()
-        algorithm.update(b"")
-        assert algorithm.digest() == copy.digest()
-        assert algorithm.hexdigest() == copy.hexdigest()
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md2"), reason="requires MD2 support in libmbedtls"
-)
-class TestHashMD2(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.md2()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 16
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md4"), reason="requires MD4 support in libmbedtls"
-)
-class TestHashMD4(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.md4()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md5"), reason="requires MD5 support in libmbedtls"
-)
-class TestHashMD5(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.md5()
-
-    @pytest.fixture
-    def type_(self):
-        return 3
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha1"),
-    reason="requires SHA1 support in libmbedtls",
-)
-class TestHashSHA1(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.sha1()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 20
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha256"),
-    reason="requires SHA256 support in libmbedtls",
-)
-class TestHashSHA224(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.sha224()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 28
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha256"),
-    reason="requires SHA256 support in libmbedtls",
-)
-class TestHashSHA256(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.sha256()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 32
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha512"),
-    reason="requires SHA384 support in libmbedtls",
-)
-class TestHashSHA384(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.sha384()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 48
-
-    @pytest.fixture
-    def block_size(self):
-        return 128
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha512"),
-    reason="requires SHA512 support in libmbedtls",
-)
-class TestHashSHA512(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.sha512()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 64
-
-    @pytest.fixture
-    def block_size(self):
-        return 128
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("ripemd160"),
-    reason="requires RIPEMD160 support in libmbedtls",
-)
-class TestHashRIPEMD160(_TestHash):
-    @pytest.fixture
-    def algorithm(self):
-        return hashlib.ripemd160()
-
-    @pytest.fixture
-    def digest_size(self):
-        return 20
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-class _TestHmac(_TestMDBase):
-    @pytest.fixture(params=[0, 16])
-    def key(self, request, randbytes):
-        return randbytes(request.param)
-
-    @pytest.fixture
-    def buffer(self, randbytes):
-        return randbytes(512)
-
-    def test_new(self, algorithm, key, buffer):
-        copy = hmaclib.new(key, buffer, algorithm.name)
-        algorithm.update(buffer)
-        assert algorithm.digest() == copy.digest()
-        assert algorithm.hexdigest() == copy.hexdigest()
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md2"), reason="requires MD2 support in libmbedtls"
-)
-class TestHmacMD2(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.md2(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 16
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md4"), reason="requires MD4 support in libmbedtls"
-)
-class TestHmacMD4(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.md4(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("md5"), reason="requires MD5 support in libmbedtls"
-)
-class TestHmacMD5(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.md5(key)
-
-    @pytest.fixture
-    def type_(self):
-        return 3
-
-    @pytest.fixture
-    def digest_size(self):
-        return 16
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha1"),
-    reason="requires SHA1 support in libmbedtls",
-)
-class TestHmacSHA1(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.sha1(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 20
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha256"),
-    reason="requires SHA256 support in libmbedtls",
-)
-class TestHmacSHA224(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.sha224(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 28
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha256"),
-    reason="requires SHA256 support in libmbedtls",
-)
-class TestHmacSHA256(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.sha256(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 32
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha512"),
-    reason="requires SHA512 support in libmbedtls",
-)
-class TestHmacSHA384(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.sha384(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 48
-
-    @pytest.fixture
-    def block_size(self):
-        return 128
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("sha512"),
-    reason="requires SHA512 support in libmbedtls",
-)
-class TestHmacSHA512(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.sha512(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 64
-
-    @pytest.fixture
-    def block_size(self):
-        return 128
-
-
-@pytest.mark.skipif(
-    not mbedtls.has_feature("ripemd160"),
-    reason="requires RIPEMD160 support in libmbedtls",
-)
-class TestHmacRIPEMD160(_TestHmac):
-    @pytest.fixture
-    def algorithm(self, key):
-        return hmaclib.ripemd160(key)
-
-    @pytest.fixture
-    def digest_size(self):
-        return 20
-
-    @pytest.fixture
-    def block_size(self):
-        return 64
+    def test_accessors(self, algorithm):
+        obj = hashlib.new(algorithm, b"")
+
+        assert obj.digest_size == SUPPORTED_SIZES[algorithm].digest_size
+        assert obj.block_size == SUPPORTED_SIZES[algorithm].block_size
+
+    def test_new_and_update(self, algorithm, randbytes):
+        buffer = randbytes(512)
+
+        obj = hashlib.new(algorithm, b"")
+        obj.update(buffer)
+        other = hashlib.new(algorithm, buffer)
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
+
+    def test_update_and_copy(self, algorithm, randbytes):
+        buffer = randbytes(512)
+
+        obj = hashlib.new(algorithm, buffer)
+        other = obj.copy()
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
+
+    def test_copy_and_update(self, algorithm, randbytes):
+        buffer = randbytes(512)
+
+        obj = hashlib.new(algorithm, buffer)
+        other = obj.copy()
+        obj.update(buffer)
+        other.update(buffer)
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
+
+    def test_copy_and_update_nothing(self, algorithm, randbytes):
+        buffer = randbytes(512)
+
+        obj = hashlib.new(algorithm, buffer)
+        other = obj.copy()
+        obj.update(b"")
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
+
+
+class TestHmac:
+    @pytest.fixture(params=tuple(hmaclib.algorithms_available))
+    def algorithm(self, request):
+        return request.param
+
+    @pytest.mark.parametrize("repr_", (repr, str), ids=lambda f: f.__name__)
+    def test_repr(self, repr_, algorithm):
+        assert isinstance(repr_(hmaclib.new(b"", digestmod=algorithm)), str)
+
+    def test_pickle(self, algorithm):
+        with pytest.raises(TypeError) as excinfo:
+            pickle.dumps(hmaclib.new(b"", digestmod=algorithm))
+
+        assert str(excinfo.value).startswith("cannot pickle")
+
+    def test_accessors(self, algorithm):
+        obj = hmaclib.new(b"", digestmod=algorithm)
+
+        assert obj.digest_size == SUPPORTED_SIZES[algorithm].digest_size
+        assert obj.block_size == SUPPORTED_SIZES[algorithm].block_size
+
+    def test_new_and_update(self, algorithm, randbytes):
+        key = b"key"
+        buffer = randbytes(512)
+
+        obj = hmaclib.new(key, b"", digestmod=algorithm)
+        obj.update(buffer)
+        other = hmaclib.new(key, buffer, digestmod=algorithm)
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
+
+    def test_copy_and_update_nothing(self, algorithm, randbytes):
+        key = b"key"
+        buffer = randbytes(512)
+
+        obj = hmaclib.new(key, buffer, digestmod=algorithm)
+        other = hmaclib.new(key, buffer, digestmod=algorithm)
+        obj.update(b"")
+
+        assert obj.digest() == other.digest()
+        assert obj.hexdigest() == other.hexdigest()
