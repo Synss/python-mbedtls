fa93d141e27ec63b1c73b8ba36b1d8fb858fcd97
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-06-18 14:53:16 +0200

    test_version: Add type annotations

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,17 +3,17 @@ import pytest
 from mbedtls import version
 
 
-def test_version_info():
+def test_version_info() -> None:
     assert len(version.version_info) == 3
     assert version.version_info >= (2, 7, 9)
 
 
-def test_version():
+def test_version() -> None:
     assert version.version == "mbed TLS %i.%i.%i" % version.version_info
 
 
 @pytest.mark.parametrize("feature", ["i do not exist"])
-def test_feature_false(feature):
+def test_feature_false(feature: str) -> None:
     assert version.has_feature(feature) is False
 
 
@@ -30,5 +30,5 @@ def test_feature_false(feature):
         "MBEDTLS_MD5_C",
     ],
 )
-def test_feature_true(feature):
+def test_feature_true(feature: str) -> None:
     assert version.has_feature(feature) is True
