e80943b3f5f6ce9dcbac9e25cf416771982f6428
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-07-20 21:29:53 +0200

    ci: Increase version to 2.3.0

--- src/mbedtls/__init__.py
+++ src/mbedtls/__init__.py
@@ -16,7 +16,7 @@ import mbedtls.tls as tls
 import mbedtls.version as version  # type: ignore
 import mbedtls.x509 as x509
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 __all__ = (
     "cipher",
