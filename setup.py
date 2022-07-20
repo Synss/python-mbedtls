daf34241f7e7e583cd696d802f87fa1d36b6dcf1
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-07-20 21:20:24 +0200

    setup.py: Update classifiers

--- setup.py
+++ setup.py
@@ -198,10 +198,10 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Cython",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Topic :: Security :: Cryptography",
     ],
