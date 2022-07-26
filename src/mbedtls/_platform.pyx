53894b529c549193439b94b689bf683916001163
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-07-26 22:15:24 +0200

    platform: Extract self test

--- src/mbedtls/_platform.pyx
+++ src/mbedtls/_platform.pyx
@@ -11,15 +11,3 @@ __all__ = ["zeroize"]
 
 def zeroize(char[:] buffer not None):
     _plt.mbedtls_platform_zeroize(&buffer[0], buffer.size)
-
-
-def __self_test():
-    binary = bytearray(b"0123456789abcdef")
-    cdef unsigned char[:] c_binary = binary
-    zeroize(c_binary)
-    assert binary == b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", binary
-
-    string = bytearray(b"0123456789abcdef")
-    cdef char[:] c_string = string
-    zeroize(c_string)
-    assert string == b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", string
--- tests/test_platform.py
+++ tests/test_platform.py
@@ -1,5 +1,11 @@
 import mbedtls._platform as _plt
 
 
-def test_platform() -> None:
-    _plt.__self_test()
+def test_zeroize_bytes() -> None:
+    binary = bytearray(b"0123456789abcdef")
+    length = len(binary)
+
+    _plt.zeroize(binary)
+
+    assert len(binary) == length
+    assert binary == b"\0" * length
