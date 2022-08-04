f3567a6a812ef54e80f957c8d8108d289cbe9ba5
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-08-04 13:04:09 +0200

    scripts: Add `update-reqs` script
    Automates the boring stuff.

---
+++ scripts/update-reqs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+die() {
+    printf "%s\n" "$*"
+    exit 1
+}
+
+command -v pip-compile 1> /dev/null || die "pip-tools is missing"
+
+for input in setup.py requirements/*.in; do
+    pip-compile --upgrade --no-annotate "$input"
+done
