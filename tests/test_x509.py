2e4352b26b41076d1e7964ddb8e85fd8af03a0f9
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-08-07 20:41:58 +0200

    test_x509: Simplify tests with less fixtures

--- tests/test_x509.py
+++ tests/test_x509.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import datetime as dt
 import pickle
 import sys
-from typing import Any, Callable, Optional, Tuple, Union, cast
+from typing import Any, Callable, Optional, Tuple, Union
 
 import certifi
 import pytest
@@ -87,15 +87,56 @@ def make_root_ca(
     else:
         csr, key = csr_key_pair
 
-    crt = CRT.selfsign(
-        csr=csr,
-        issuer_key=key,
-        not_before=not_before,
-        not_after=not_after,
-        serial_number=serial_number,
-        basic_constraints=basic_constraints,
+    return (
+        CRT.selfsign(
+            csr=csr,
+            issuer_key=key,
+            not_before=not_before,
+            not_after=not_after,
+            serial_number=serial_number,
+            basic_constraints=basic_constraints,
+        ),
+        key,
     )
-    return crt, key
+
+
+def make_crt(
+    issuer_crt: CRT,
+    issuer_key: _PKey,
+    subject: Optional[str] = None,
+    not_before: Optional[dt.datetime] = None,
+    not_after: Optional[dt.datetime] = None,
+    serial_number: Optional[int] = None,
+    basic_constraints: Optional[BasicConstraints] = None,
+    digestmod: Optional[hashlib.Algorithm] = None,
+    csr_key_pair: Optional[Tuple[CSR, _PKey]] = None,
+) -> Tuple[CRT, _PKey]:
+    if subject is None:
+        subject = "OU=test, CN=Intermediate Cert"
+    if not_before is None:
+        not_before = dt.datetime.utcnow()
+    if not_after is None:
+        not_after = not_before + dt.timedelta(days=90)
+    if serial_number is None:
+        serial_number = 0x123456
+    if basic_constraints is None:
+        basic_constraints = BasicConstraints(True, -1)
+    if digestmod is None:
+        digestmod = hashlib.sha256
+    if csr_key_pair is None:
+        csr, key = make_csr(subject=subject, digestmod=digestmod)
+
+    return (
+        issuer_crt.sign(
+            csr,
+            issuer_key,
+            not_before,
+            not_after,
+            serial_number,
+            basic_constraints,
+        ),
+        key,
+    )
 
 
 def make_crl() -> CRL:
@@ -163,55 +204,26 @@ class TestCertificate:
 
 
 class TestCRT:
-    @pytest.fixture()
-    def issuer(self) -> str:
-        return "C=NL, O=PolarSSL, CN=PolarSSL Test CA"
-
-    @pytest.fixture()
-    def issuer_key(self) -> _PKey:
+    @pytest.mark.parametrize(
+        "basic_constraints", [(True, 0), (True, 2), (False, 0)]
+    )
+    def test_accessor(
+        self, now: dt.datetime, basic_constraints: BasicConstraints
+    ) -> None:
+        not_before = now
+        not_after = now + dt.timedelta(days=90)
+        issuer = "C=NL, O=PolarSSL, CN=PolarSSL Test CA"
         issuer_key = RSA()
         issuer_key.generate(key_size=1024)
-        return issuer_key
-
-    @pytest.fixture()
-    def subject(self) -> str:
-        return "C=NL"
-
-    @pytest.fixture()
-    def subject_key(self) -> _PKey:
+        subject = "C=NL"
         subject_key = RSA()
         subject_key.generate(key_size=1024)
-        return subject_key
+        serial_number = 0x1234567890
+        digestmod = hashlib.sha256()
 
-    @pytest.fixture()
-    def serial_number(self) -> int:
-        return 0x1234567890
-
-    @pytest.fixture(params=[hashlib.sha1, hashlib.sha256])
-    def digestmod(self, request: Any) -> hashlib.Hash:
-        digestmod = request.param()
-        assert isinstance(digestmod, hashlib.Hash)
-        return digestmod
-
-    @pytest.fixture(params=[(True, 0), (True, 2), (False, 0), None])
-    def basic_constraints(self, request: Any) -> Tuple[bool, int]:
-        return cast(Tuple[bool, int], request.param)
-
-    @pytest.fixture()
-    def crt(
-        self,
-        now: dt.datetime,
-        issuer: str,
-        issuer_key: _PKey,
-        subject: str,
-        subject_key: _PKey,
-        serial_number: int,
-        digestmod: hashlib.Hash,
-        basic_constraints: BasicConstraints,
-    ) -> CRT:
-        return CRT.new(
-            not_before=now,
-            not_after=now + dt.timedelta(days=90),
+        crt = CRT.new(
+            not_before=not_before,
+            not_after=not_after,
             issuer=issuer,
             issuer_key=issuer_key,
             subject=subject,
@@ -221,33 +233,28 @@ class TestCRT:
             basic_constraints=basic_constraints,
         )
 
-    def test_version(self, crt: CRT) -> None:
-        assert crt.version == 3
-
-    def test_not_before(self, crt: CRT, now: dt.datetime) -> None:
-        assert crt.not_before == now
-
-    def test_not_after(self, crt: CRT, now: dt.datetime) -> None:
-        assert crt.not_after == now + dt.timedelta(days=90)
-
-    def test_issuer(self, crt: CRT, issuer: str) -> None:
+        assert crt.not_before == not_before
+        assert crt.not_after == not_after
         assert crt.issuer == issuer
+        assert crt.serial_number == serial_number
+        assert crt.subject == "C=NL"
+        assert crt.subject_public_key == subject_key.export_public_key(
+            format="DER"
+        )
+        assert crt.basic_constraints == basic_constraints
 
-    def test_public_key(self, crt: CRT) -> None:
+    def test_public_key(self) -> None:
+        crt, key = make_root_ca()
         pem = crt.subject_public_key.export_public_key(format="PEM")
         assert pem.startswith("-----BEGIN PUBLIC KEY-----\n")
         assert pem.rstrip("\0").endswith("-----END PUBLIC KEY-----\n")
+        assert pem == key.export_public_key(format="PEM")
 
-    def test_subject(self, crt: CRT, subject: str) -> None:
-        assert crt.subject == subject
-
-    def test_serial_number(self, crt: CRT, serial_number: int) -> None:
-        assert crt.serial_number == serial_number
-
-    def test_revocation_bad_cast(self, crt: CRT) -> None:
-        crt = CRT.from_buffer(crt.to_DER())
+    def test_revocation_bad_cast(self) -> None:
+        crt, _key = make_root_ca()
+        copy = CRT.from_buffer(crt.to_DER())
         with pytest.raises(TypeError):
-            crt.check_revocation(crt)  # type: ignore[arg-type]
+            copy.check_revocation(crt)  # type: ignore[arg-type]
 
     def test_next(self) -> None:
         crt = CRT.from_file(certifi.where())
@@ -255,67 +262,41 @@ class TestCRT:
             while True:
                 crt = next(crt)
 
-    def test_digestmod(self, crt: CRT, digestmod: hashlib.Hash) -> None:
-        assert crt.digestmod.name == digestmod.name
-
-    def test_ca(self, crt: CRT, basic_constraints: BasicConstraints) -> None:
-        if basic_constraints is None:
-            basic_constraints = BasicConstraints()
-        assert crt.basic_constraints == basic_constraints
-
 
 class TestCSR:
-    @pytest.fixture()
-    def subject(self) -> str:
-        return "C=NL, O=PolarSSL, CN=PolarSSL Server 1"
-
-    @pytest.fixture()
-    def subject_key(self) -> _PKey:
+    def test_accessors(self) -> None:
+        subject = "C=NL, O=PolarSSL, CN=PolarSSL Server 1"
         subject_key = RSA()
         subject_key.generate(key_size=1024)
-        return subject_key
 
-    @pytest.fixture()
-    def csr(self, subject: str, subject_key: _PKey) -> CSR:
-        return CSR.new(subject_key, subject, hashlib.sha1())
+        csr, _k = make_csr(
+            key=subject_key,
+            subject=subject,
+        )
 
-    def test_version(self, csr: CSR) -> None:
         assert csr.version == 1
-
-    def test_subject(self, csr: CSR, subject: str) -> None:
         assert csr.subject == subject
-
-    def test_subject_public_key(self, csr: CSR, subject_key: _PKey) -> None:
         assert csr.subject_public_key == subject_key.export_public_key()
 
 
 class TestCRL:
-    @pytest.fixture()
-    def crl(self) -> CRL:
-        return CRL.from_PEM(CRL_PEM)
+    def test_accessors(self) -> None:
+        crl = make_crl()
 
-    def test_tbs_certificate(self, crl: CRL) -> None:
+        assert crl.version == 1
+        assert crl.issuer_name == "C=NL, O=PolarSSL, CN=PolarSSL Test CA"
+        assert crl.this_update == dt.datetime(2011, 2, 20, 10, 22, 59)
+        assert crl.next_update == dt.datetime(2019, 11, 25, 10, 22, 59)
+
         assert isinstance(crl.tbs_certificate, bytes)
         assert crl.tbs_certificate
-
-    def test_signature_value(self, crl: CRL) -> None:
         assert isinstance(crl.signature_value, bytes)
         assert crl.signature_value
 
-    def test_version(self, crl: CRL) -> None:
-        assert crl.version == 1
-
-    def test_issuer_name(self, crl: CRL) -> None:
-        assert crl.issuer_name == "C=NL, O=PolarSSL, CN=PolarSSL Test CA"
-
-    def test_this_update(self, crl: CRL) -> None:
-        assert crl.this_update == dt.datetime(2011, 2, 20, 10, 22, 59)
-
-    def test_next_update(self, crl: CRL) -> None:
-        assert crl.next_update == dt.datetime(2019, 11, 25, 10, 22, 59)
-
-    def test_revoked_certificates(self, crl: CRL) -> None:
+    def test_revoked_certificates(self) -> None:
+        crl = make_crl()
         assert len(crl.revoked_certificates) == 2
+
         entry = crl.revoked_certificates[0]
         assert entry.revocation_date == dt.datetime(2011, 2, 12, 14, 44, 7)
         assert entry.serial == 1
@@ -333,60 +314,11 @@ class TestCRL:
 
 
 class TestVerifyCertificateChain:
-    @pytest.fixture()
-    def ca0_key(self) -> _PKey:
-        ca0_key = RSA()
-        ca0_key.generate()
-        return ca0_key
+    def test_verify_chain(self) -> None:
+        crt0, key0 = make_root_ca()
+        crt1, key1 = make_crt(crt0, key0)
+        crt2, key2 = make_crt(crt1, key1)
 
-    @pytest.fixture()
-    def ca1_key(self) -> _PKey:
-        ca1_key = ECC()
-        ca1_key.generate()
-        return ca1_key
-
-    @pytest.fixture()
-    def ee0_key(self) -> _PKey:
-        ee0_key = ECC()
-        ee0_key.generate()
-        return ee0_key
-
-    @pytest.fixture()
-    def ca0_crt(self, ca0_key: _PKey, now: dt.datetime) -> CRT:
-        ca0_csr = CSR.new(ca0_key, "CN=Trusted CA", hashlib.sha256())
-        return CRT.selfsign(
-            ca0_csr,
-            ca0_key,
-            not_before=now,
-            not_after=now + dt.timedelta(days=90),
-            serial_number=0x123456,
-            basic_constraints=BasicConstraints(True, -1),
-        )
-
-    @pytest.fixture()
-    def ca1_crt(
-        self, ca1_key: _PKey, ca0_crt: CRT, ca0_key: _PKey, now: dt.datetime
-    ) -> CRT:
-        ca1_csr = CSR.new(ca1_key, "CN=Intermediate CA", hashlib.sha256())
-        return ca0_crt.sign(
-            ca1_csr,
-            ca0_key,
-            now,
-            now + dt.timedelta(days=90),
-            0x234567,
-            basic_constraints=BasicConstraints(True, 1),
-        )
-
-    @pytest.fixture()
-    def ee0_crt(
-        self, ee0_key: _PKey, ca1_crt: CRT, ca1_key: _PKey, now: dt.datetime
-    ) -> CRT:
-        ee0_csr = CSR.new(ee0_key, "CN=End Entity", hashlib.sha256())
-        return ca1_crt.sign(
-            ee0_csr, ca1_key, now, now + dt.timedelta(days=90), 0x345678
-        )
-
-    def test_verify_chain(
-        self, ca0_crt: CRT, ca1_crt: CRT, ee0_crt: CRT
-    ) -> None:
-        assert all((ca1_crt.verify(ee0_crt), ca0_crt.verify(ca0_crt)))
+        assert crt0.verify(crt0) is True
+        assert crt1.verify(crt2) is True
+        assert crt2.verify(crt0) is False
