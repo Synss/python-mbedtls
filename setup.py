53d9aae8800286ec40ec3461e5a01f961374204a
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-09-07 18:32:35 +0200

    cipher: Add missing pyi file to wheel

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ setup(
     download_url=DOWNLOAD_URL,
     ext_modules=list(extensions(COVERAGE)),
     options=options(COVERAGE),
-    package_data={"mbedtls": ["py.typed", "**.pyi"]},
+    package_data={"mbedtls": ["py.typed", "**.pyi", "cipher/**pyi"]},
     package_dir={"": "src"},
     packages=find_packages("src"),
     setup_requires=setup_requires,
