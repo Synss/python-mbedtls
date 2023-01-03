a63d33f80171a71b4ae957a6c11dcaf73d41658d
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2023-01-03 21:32:52 +0100

    ci: Update backend to 2.28.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def _get_version():
 
 
 VERSION = _get_version()
-MBEDTLS_VERSION = "2.28.1"
+MBEDTLS_VERSION = "2.28.2"
 DOWNLOAD_URL = f"https://github.com/Synss/python-mbedtls/tarball/{VERSION}"
 
 
