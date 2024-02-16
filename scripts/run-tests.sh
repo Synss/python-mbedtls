ce95f3abba98bf3328bc6b6a9feefee4426bb475
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2024-02-16 12:07:58 +0100

    scripts: Fixup run-tests.sh
    Make sure the script only runs *our* tests.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,5 +7,5 @@ echo "Index: $PIP_INDEX_URL"
 python="${1:-python}"
 $python -m pip install -r requirements/tests.txt
 $python -m pip install --only-binary=:all: python-mbedtls
-$python -B -m pytest --color=yes
+$python -B -m pytest --color=yes tests/
 $python -B -m doctest README.rst
